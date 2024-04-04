47ee7151388b1f690b73fe95d92c1b935bbabe50
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-03 23:53:23 -0600

    Fix tests for market poro

--- spec/poros/market_spec.rb
+++ spec/poros/market_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Market' do
  describe 'Initialize' do
   before(:each) do
-    @data = {name: "14&U Farmers' Market", state: "District of Columbia", city: "Washington"}
+    @data = {name: "14&U Farmers' Market", state: "District of Columbia", city: "Washington", id: "322458"}
   end
 
   it 'creates markets populating attributes correctly' do
@@ -13,6 +13,7 @@ RSpec.describe 'Market' do
     expect(market.name).to eq("14&U Farmers' Market")
     expect(market.state).to eq("District of Columbia")
     expect(market.city).to eq("Washington")
+    expect(market.id).to eq(322458)
   end
  end
 end
