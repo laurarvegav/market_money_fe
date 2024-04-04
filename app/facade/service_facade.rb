004368006e1869a88d46bc23e29e6ae494c11334
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-04 00:51:36 -0600

    Refactor methods markets and market with helper method market_data

--- app/facade/service_facade.rb
+++ app/facade/service_facade.rb
@@ -5,14 +5,29 @@ class ServiceFacade
 
     json = service.find_markets
     json[:data].map do |market_data|
-      data = {
-        id: market_data[:id],
-        name: market_data[:attributes][:name],
-        state: market_data[:attributes][:state],
-        city: market_data[:attributes][:city]
-      }
+      data = market_data(market_data)
 
       Market.new(data)
     end
+  end
+
+  def market(id)
+    service = MarketService.new
+
+    json = service.find_market(id)
+    data = market_data(json[:data])
+
+    Market.new(data)
+  end
+
+  def market_data(market_data)
+    data = {
+      id: market_data[:id],
+      name: market_data[:attributes][:name],
+      state: market_data[:attributes][:state],
+      city: market_data[:attributes][:city],
+      street: market_data[:attributes][:street],
+      zip: market_data[:attributes][:zip]
+    }
   end
 end
--- spec/facade/service_facade_spec.rb
+++ spec/facade/service_facade_spec.rb
@@ -1,18 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe 'Service Facade' do
- describe 'Instance methods' do
-  describe 'markets' do
-    it 'returns an array of market data' do
-      markets = ServiceFacade.new.markets
-      expect(markets).to be_an(Array)
-      markets.each do |market|
+  describe 'Instance methods' do
+    describe 'markets' do
+      it 'returns an array of market data' do
+        markets = ServiceFacade.new.markets
+        expect(markets).to be_an(Array)
+        markets.each do |market|
+          expect(market.id).to be_an(Integer)
+          expect(market.name).to be_a(String)
+          expect(market.address).to be_a(String)
+        end
+      end
+
+      it 'returns market when given its id' do
+        id = 322458
+        market = ServiceFacade.new.market(id)
+        expect(market).to be_a(Market)
         expect(market.id).to be_an(Integer)
         expect(market.name).to be_a(String)
-        expect(market.city).to be_a(String)
-        expect(market.state).to be_a(String)
+        expect(market.address).to be_a(String)
       end
     end
   end
- end
+
+  describe 'helper methods' do
+    describe 'market_data' do
+      it "returns a hash of market data" do
+        data = 
+        {:id=>"322458",
+          :type=>"market",
+          :attributes=>
+            {:name=>"14&U Farmers' Market",
+            :street=>"1400 U Street NW ",
+            :city=>"Washington",
+            :county=>"District of Columbia",
+            :state=>"District of Columbia",
+            :zip=>"20009",
+            :lat=>"38.9169984",
+            :lon=>"-77.0320505",
+            :vendor_count=>1},
+          :relationships=>{:vendors=>{:data=>[{:id=>"55823", :type=>"vendor"}]}}}
+
+        expect(ServiceFacade.new.market_data(data)).to eq({name: "14&U Farmers' Market", street: "1400 U Street NW ",
+        city: "Washington",
+        state: "District of Columbia",
+        zip: "20009", id: "322458"})
+
+      end
+    end
+  end
 end
