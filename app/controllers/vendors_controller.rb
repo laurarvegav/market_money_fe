23bb575b2b2b141b2aea15f5acc9b38f6a06202c
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-04 16:00:44 -0600

    Add helper methods and update show action

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,5 +1,31 @@
 class VendorsController < ApplicationController
   def show
     @vendor = ServiceFacade.new.vendor(params[:id])
+    if search_params.present? && search_input.present?
+      @markets = ServiceFacade.new.markets_search(search_input) 
+    else
+      flash[:error] = "Name or city can not be sent on their own. Please provide other parameters to search for markets"
+    end
+  end
+
+  private
+  def search_params
+    (params[:name].present? || params[:city].present? || params[:state].present? ) && (params[:name]!="Name" || params[:city]!="City" || params[:state]!="State")
+  end
+
+  def search_input
+    if params[:name]!="Name" && params[:city]!="City" && params[:state]!="State"
+      "name=#{params[:name]}&state=#{params[:state]}&city=#{params[:city]}"
+    elsif params[:name]!="Name" && params[:state]!="State"
+      "name=#{params[:name]}&state=#{params[:state]}"
+    elsif params[:name]!="Name" && params[:city]!="City"
+      "name=#{params[:name]}&city=#{params[:city]}"
+    elsif params[:state]!="State" && params[:city]!="City"
+      "state=#{params[:state]}&city=#{params[:city]}"
+    elsif params[:state]!="State"
+      "state=#{params[:state]}"
+    else
+      nil
+    end
   end
 end
