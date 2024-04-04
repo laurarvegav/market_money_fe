707e814f3040f439cf66fa48630d0123d11329aa
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-03 23:53:37 -0600

    Fix market poro

--- app/poros/market.rb
+++ app/poros/market.rb
@@ -1,9 +1,11 @@
 class Market
-  attr_reader :name,
+  attr_reader :id,
+              :name,
               :state,
               :city
   
   def initialize(data)
+    @id = data[:id].to_i
     @name = data[:name]
     @state = data[:state]
     @city = data[:city]
