519d251757063c70cf4b6de707a576df993e7e46
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-04 01:10:00 -0600

    Create vendor method

--- app/facade/service_facade.rb
+++ app/facade/service_facade.rb
@@ -20,6 +20,22 @@ class ServiceFacade
     Market.new(data)
   end
 
+  def vendor(id)
+    service = MarketService.new
+
+    json = service.find_vendor(id)
+    data = {
+      id: json[:data][:id],
+      name: json[:data][:attributes][:name],
+      description: json[:data][:attributes][:description],
+      contact_name: json[:data][:attributes][:contact_name],
+      contact_phone: json[:data][:attributes][:contact_phone],
+      credit_accepted: json[:data][:attributes][:credit_accepted]
+    }
+
+    Vendor.new(data)
+  end
+
   def market_data(market_data)
     data = {
       id: market_data[:id],
@@ -27,7 +43,10 @@ class ServiceFacade
       state: market_data[:attributes][:state],
       city: market_data[:attributes][:city],
       street: market_data[:attributes][:street],
-      zip: market_data[:attributes][:zip]
+      zip: market_data[:attributes][:zip],
+      vendors_ids: market_data[:relationships][:vendors][:data].map do |vendor|
+        vendor[:id]
+      end
     }
   end
 end
