fa7d96608ce326bf573b56c832eff828b392bb16
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-04 00:44:20 -0600

    Fix tests for Market poro regarding its attributes and new method address

--- spec/poros/market_spec.rb
+++ spec/poros/market_spec.rb
@@ -3,17 +3,26 @@ require 'rails_helper'
 RSpec.describe 'Market' do
  describe 'Initialize' do
   before(:each) do
-    @data = {name: "14&U Farmers' Market", state: "District of Columbia", city: "Washington", id: "322458"}
+    @data = {name: "14&U Farmers' Market", street: "1400 U Street NW",
+    city: "Washington",
+    state: "District of Columbia",
+    zip: "20009", id: "322458"}
+    @market = Market.new(@data)
   end
 
   it 'creates markets populating attributes correctly' do
-    market = Market.new(@data)
 
-    expect(market).to be_a(Market)
-    expect(market.name).to eq("14&U Farmers' Market")
-    expect(market.state).to eq("District of Columbia")
-    expect(market.city).to eq("Washington")
-    expect(market.id).to eq(322458)
+    expect(@market).to be_a(Market)
+    expect(@market.name).to eq("14&U Farmers' Market")
+    expect(@market.street).to eq("1400 U Street NW")
+    expect(@market.city).to eq("Washington")
+    expect(@market.state).to eq("District of Columbia")
+    expect(@market.zip).to eq("20009")
+    expect(@market.id).to eq(322458)
+  end
+
+  it 'returns a readable address' do
+    expect(@market.address).to eq("1400 U Street NW Washington, District of Columbia 20009")
   end
  end
 end
