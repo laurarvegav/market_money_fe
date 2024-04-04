38db2b236438147d91ba369071e7fce8a91202a0
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-04 00:21:36 -0600

    Test: create tests for find_market and find_vendor

--- spec/services/market_service_spec.rb
+++ spec/services/market_service_spec.rb
@@ -20,5 +20,30 @@ RSpec.describe 'MarketService' do
     expect(parsed_markets[:data].first[:attributes][:city]).to be_a(String)
     expect(parsed_markets[:data].first[:attributes][:state]).to be_a(String)
   end
+
+  it 'finds a market given its id' do
+    id = 322458
+    parsed_market = MarketService.new.find_market(id)
+
+    expect(parsed_market[:data]).to be_a(Hash)
+    expect(parsed_market[:data][:attributes][:name]).to be_a(String)
+    expect(parsed_market[:data][:attributes][:city]).to be_a(String)
+    expect(parsed_market[:data][:attributes][:state]).to be_a(String)
+    expect(parsed_market[:data][:id]).to be_a(String)
+    expect(parsed_market[:data][:relationships][:vendors][:data].first[:id]).to be_a(String)
+  end
+
+  it 'finds a vendor given its id' do
+    id = 55823
+    parsed_vendor = MarketService.new.find_vendor(id)
+
+    expect(parsed_vendor[:data]).to be_a(Hash)
+    expect(parsed_vendor[:data][:attributes][:name]).to be_a(String)
+    expect(parsed_vendor[:data][:attributes][:description]).to be_a(String)
+    expect(parsed_vendor[:data][:attributes][:contact_name]).to be_a(String)
+    expect(parsed_vendor[:data][:attributes][:contact_phone]).to be_a(String)
+    expect(parsed_vendor[:data][:attributes][:credit_accepted]).to be_in([true, false])
+    expect(parsed_vendor[:data][:id]).to be_a(String)
+  end
  end
 end
